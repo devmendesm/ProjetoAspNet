ed7688c24f8619d351374ceeadae471ec9cef343
devmendesm <matheusviniciusmendes@gmail.com>
2026-05-10 16:44:42 -0300

    FEAT:form cadastro de vendedores e suas operações (NAO CADASTRA AINDA DEVIDO FK DO BD)

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
+        // Dependencia com a SellerService
         private readonly SellerService _sellerService;
 
         public SellersController(SellerService sellerService)
@@ -17,6 +19,19 @@ namespace SalesWebMvc.Controllers
             var list = _sellerService.FindAll();
 
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] // Impede Ataque XSRF
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
